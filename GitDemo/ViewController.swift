f7edad855401e1f2be9405bfb1e32f4b0dbc1ada
leiweiz <zhengleiwei@hotmail.com>
2018-03-22 20:59:20 -0400

    revert

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello, world")
-        print("hello cs193p")
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
 }
